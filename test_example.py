2f1f29ce8ff96aa732fc93ee3af3b7b9d5ce87a7
rduarte2047 <rogerio.duarte@estsetubal.ips.pt>
2019-04-16 21:57:04 +0100

    Including new file with elementary operations

---
+++ test_example.py
@@ -0,0 +1,17 @@
+def sum(x,y):
+    return x+y
+
+def subtract(x,y):
+    return x-y
+
+def multiply(x,y):
+    return x*y
+
+def divide(x,y):
+    try:
+        return x/y
+    except ValueError:
+        print("Error in division")
+
+x, y = 1, 2
+print(sum(x,y))
